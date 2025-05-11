c4c23dc0defde3d8710de44541a84ed0d752f82b
Hunter Ver Helst <30808586+hunterverhelst@users.noreply.github.com>
2025-05-11 13:48:55 -0400

    update dependencies, add keybase

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,6 +1,7 @@
 import { CHANNEL_TYPES } from './packages/Linkees/src/constants'
 import Linkees from './packages/Linkees/src/index';
 import { faFilePdf } from '@fortawesome/free-regular-svg-icons';
+import { faKeybase } from '@fortawesome/free-brands-svg-icons'
 import './App.css';
 import { ItemType } from './packages/Linkees/src/ts';
 
@@ -20,17 +21,25 @@ const cards: ItemType[] = [
     link: 'https://www.linkedin.com/in/hunterverhelst/'
   },
   {
+    title: "Keybase",
+    subtitle: "keybase.io/hunterverhelst",
+    type: CHANNEL_TYPES.OTHER,
+    image: faKeybase,
+    link: 'https://keybase.io/hunterverhelst'
+  },
+  {
     title: 'Resume',
     subtitle: 'Resume.pdf',
     type: CHANNEL_TYPES.OTHER,
     image: faFilePdf,
     link: process.env.PUBLIC_URL + '/Resume.pdf'
-  },
+  }
+
 ]
 
 function App() {
   return (
-      <Linkees name="Hunter Ver Helst" cardItems={cards} headerAvatar={linkedin_pfp}/>
+    <Linkees name="Hunter Ver Helst" cardItems={cards} headerAvatar={linkedin_pfp} />
   );
 }
 
